f6e5e80107f0c3068a2f16377fec00ddd1064b22
Desmond O'Leary <desoleary@gmail.com>
2024-09-26 12:25:37 -0600

    refactored ArrayUtils::undot to use Illuminate\Support\Arr::set instead of custom implementation

--- src/Utilify/ArrayUtils.php
+++ src/Utilify/ArrayUtils.php
@@ -2,6 +2,8 @@
 
 namespace Omnitech\Utilify;
 
+use Illuminate\Support\Arr;
+
 class ArrayUtils
 {
     /**
@@ -14,32 +16,9 @@ class ArrayUtils
     {
         $array = [];
         foreach ($dotNotationArray as $key => $value) {
-            self::arraySet($array, $key, $value);
+            Arr::set($array, $key, $value);
         }
 
         return $array;
-    }
-
-    /**
-     * Set an array value using "dot" notation for the key.
-     *
-     * @param  array<string, mixed>  $array  The reference array to set values in.
-     * @param  string  $key  The dot notation key.
-     * @param  mixed  $value  The value to set in the array.
-     */
-    protected static function arraySet(array &$array, string $key, mixed $value): void
-    {
-        $keys = explode('.', $key);
-        while (count($keys) > 1) {
-            $key = array_shift($keys);
-
-            if (! isset($array[$key]) || ! is_array($array[$key])) {
-                $array[$key] = [];
-            }
-
-            $array = &$array[$key];
-        }
-
-        $array[array_shift($keys)] = $value;
     }
 }
